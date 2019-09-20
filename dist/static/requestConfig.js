d523831079bf60c69ac18a46e2389f422c9a2b19
specialname8 <specialname8@hotmail.com>
2019-09-20 19:09:24 +0800

    macau-test-init

---
+++ dist/static/requestConfig.js
@@ -0,0 +1,8 @@
+window.g={
+    IP:'http://ecpapp.fsm.gov.mo',
+	PORT:"8090/mbser/",
+ KEY:"76db547ed172fe36897e31dbd9b8257350870c9977985bfdc69690e2aca2d582",
+ //KEY:"5e9b3302bb49493f38b478e022d27db1d5a863d23893fc1b002af81647d9faa7",
+// IP:"http://10.3.10.180",
+// PORT:"8088/mbser/",
+}
