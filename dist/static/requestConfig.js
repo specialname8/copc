c9f3b9e29ebd8e927a06b662e7c85c9d7cd1b30b
specialname8 <specialname8@hotmail.com>
2019-09-24 14:48:24 +0800

    0924

--- dist/static/requestConfig.js
+++ dist/static/requestConfig.js
@@ -1,8 +1,14 @@
 window.g={
-    IP:'http://ecpapp.fsm.gov.mo',
-	PORT:"8090/mbser/",
+    IP:'http://10.3.10.180',
+	PORT:"8888/mbser/",
  KEY:"76db547ed172fe36897e31dbd9b8257350870c9977985bfdc69690e2aca2d582",
  //KEY:"5e9b3302bb49493f38b478e022d27db1d5a863d23893fc1b002af81647d9faa7",
 // IP:"http://10.3.10.180",
 // PORT:"8088/mbser/",
+test_getMessages:'./static/getMessages.json',
+test_getOneMessages:'./static/getOneMessages.json',
+test_sciencepopularizations:'./static/sciencepopularizations.json',
+// test_getMessages:'',
+// test_getOneMessages:'',
+// test_sciencepopularizations:'',
 }
